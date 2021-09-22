922a9451a535808cdd696d10c44a07cdf0fdea8f
Ernie Cruz <ecruz64@me.com>
2021-09-22 17:04:16 -0400

    up to lesson 32 up to lesson 45

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,10 +2,24 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: blue;
+    }
+  `]
 })
 export class ServerComponent {
-  serverID = 10;
+  serverID: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+  getServerStatus() {
+    return this.serverStatus;
+  }
+  getColor() {
+    return this.serverStatus == 'online' ? 'offline' : 'red';
+  }
 }
